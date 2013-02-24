cc6ca6905b2ffe7a42151cb43e48c5615a224789
Abhijit Sarkar <abhijit.sarcar@gmail.com>
2013-02-23 22:07:37 -0500

    Deleted ruby-gems

--- ruby-gems/datastructures/lib/datastructures.rb
+++
@@ -1,8 +0,0 @@
-require_relative './datastructures/graph'
-require_relative './datastructures/priority_queue'
-require_relative './datastructures/graph/vertex'
-require_relative './datastructures/graph/edge'
-
-module DataStructures
-  VERSION = "1.0.0"
-end
